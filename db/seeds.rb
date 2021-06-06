70f6b2acc345543c48be3157b6e133f39676e3e1
Walt <tronmcp@windrich.com>
2021-06-06 17:22:59 -0500

    DB Updates

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+case Rails.env
+when 'development'
+  # development-specific seeds ...
+  # (anything you need to interactively play around with in the rails console)
+
+when 'test'
+  (1..100).each do |index|
+    member = Member.create(first_name: "First Name #{index}", last_name: "Last Name #{index}")
+    member.headers.create(tag: "H1", header: "Expert in #{index}-Things")
+  end
+
+  # TODO complete test data for expert friend searcher, need lots of data.
+    
+
+  end
+
+when 'production'
+  # production seeds (if any) ...
+
+end
