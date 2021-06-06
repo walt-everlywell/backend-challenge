5c995cc3037f3bc117a13b9ecd8b6e224c46189f
Walt <tronmcp@windrich.com>
2021-06-06 17:29:33 -0500

    Config updates

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  # TODO remove un-needed routes
+  resources :friendships
+  resources :members
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
