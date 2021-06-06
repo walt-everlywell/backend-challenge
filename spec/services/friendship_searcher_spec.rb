f5dc7bd0af0ec4d87e6b51c99bf9f39b49f3f22b
Walt <tronmcp@windrich.com>
2021-06-06 16:45:11 -0500

    Services specs

---
+++ spec/services/friendship_searcher_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+describe 'Friendship Searcher' do
+  pending "add some examples to (or delete) #{__FILE__}"  
+end
---
+++ spec/services/url_shortener_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+describe 'Url Shortener' do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
