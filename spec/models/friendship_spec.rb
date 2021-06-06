1157318ecde8194877d61efb5066e23c9e1c66bf
Walt <tronmcp@windrich.com>
2021-06-06 16:38:27 -0500

    Model Specs

--- spec/models/friendship_spec.rb
+++ spec/models/friendship_spec.rb
@@ -1,13 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Friendship, type: :model do
-  it "requires a member id" do
-    friendship = Friendship.new(member_id: nil)
-    expect(friendship).to_not be_valid
-  end
+  context 'validations' do
+    it "requires a member id" do
+      friendship = Friendship.new(member_id: nil)
+      expect(friendship).to_not be_valid
+    end
 
-  it "requires a friend id" do
-    friendship = Friendship.new(friend_id: nil)
-    expect(friendship).to_not be_valid
+    it "requires a friend id" do
+      friendship = Friendship.new(friend_id: nil)
+      expect(friendship).to_not be_valid
+    end
   end
 end
--- spec/models/member_spec.rb
+++ spec/models/member_spec.rb
@@ -1,18 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Member, type: :model do
-  it "requires a first_name" do
-    member = Member.new(first_name: nil)
-    expect(member).to_not be_valid
+  context 'validations' do
+    it "requires a first_name" do
+      member = Member.new(first_name: nil)
+      expect(member).to_not be_valid
+    end
+
+    it "requires a last_name" do
+      member = Member.new(last_name: nil)
+      expect(member).to_not be_valid
+    end
+
+    it "requires a url" do
+      member = Member.new(url: nil)
+      expect(member).to_not be_valid
+    end
   end
 
-  it "requires a last_name" do
-    member = Member.new(last_name: nil)
-    expect(member).to_not be_valid
-  end
 
-  it "requires a url" do
-    member = Member.new(url: nil)
-    expect(member).to_not be_valid
-  end
+
 end
