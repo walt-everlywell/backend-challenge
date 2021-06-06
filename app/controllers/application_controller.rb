884f5e143cc09cb1e48c3a66c01b58c07103a9df
Walt <tronmcp@windrich.com>
2021-06-06 17:03:43 -0500

    Updated controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::API
+  rescue_from ActionController::ParameterMissing, with: :params_missing
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found 
+
+  private
+    def params_missing
+      render plain: "422 Unprocessable Entity", status: :unprocessable_entity
+    end
+
+    def record_not_found 
+      render plain: "404 Not Found", status: :not_found
+    end
 end
